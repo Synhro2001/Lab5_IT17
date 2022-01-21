55bd1b19f9f43ec38562d575098246ac8c1114e5
Synhro2001 <82532564+Synhro2001@users.noreply.github.com>
2022-01-21 14:46:11 +0200

    Add files via upload

---
+++ Lab5_ProgVal.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+
+
+bool ReadData(char* fileName, char* header, char* data) {
+
+	FILE* file = fopen(fileName, "rt");
+
+	if (!file) return false;
+
+
+	char str[100];
+
+	while (fgets(str, 100, file)) {
+
+		char* Head = strstr(str, header);
+
+		if (Head) {
+			sscanf(str, "%*[^:]:%[^\n]", data);
+
+			fclose(file);
+			return true;
+		}
+	}
+}
+
+
+int main() {
+
+	char fileName[100];
+	char header[100];
+	printf("Enter file name (for example Email.0.eml) = "); scanf("%s", fileName);
+	printf("\nEnter header name (for example: Return-path) = "); scanf("%s", header);
+
+	char data[100];
+
+	bool result = ReadData(fileName, header, data);
+
+	if (result) {
+
+		printf("%s:%s", header, data);
+	}
+	else {
+		printf("Header not found");
+	}
+
+
+}
